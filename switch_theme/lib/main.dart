c75f06876016b8eb37de9a6e526fca0463f30beb
Aditya Kumar <adityakumar.devxlinux@gmail.com>
2024-06-10 00:32:36 +0530

    added manage theme file

--- switch_theme/lib/main.dart
+++ switch_theme/lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
@@ -28,6 +29,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,    
         title: const Text("Theme Change"),
---
+++ switch_theme/lib/theme.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppTheme {
+  static final ThemeData lightTheme = ThemeData.light(  useMaterial3: true).copyWith(
+    brightness: Brightness.light,
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.lightBlue, brightness: Brightness.light)
+  );
+  static final ThemeData darkTheme = ThemeData.dark(useMaterial3: true).copyWith(
+    brightness: Brightness.dark,
+    
+    colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue, brightness: Brightness.dark)
+  ,
+  );
+}
